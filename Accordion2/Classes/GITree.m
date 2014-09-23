976f805078c670818655dd790ae5f4c9c88e12de
Development <support@chronicstimulation.com>
2014-09-23 13:39:23 -0400

    More changes to correct integer casting and Format String syntax errors related to move to 64-bit architecture.

--- Accordion2/Classes/GITree.m
+++ Accordion2/Classes/GITree.m
@@ -130,7 +130,7 @@
         if ([fileManager fileExistsAtPath:[node absolutePath]]) {
             NSError *error = nil;
             if (![fileManager removeItemAtPath:[node absolutePath] error:&error]) {
-                DDLogError(@"Error removing report at index: %lu ; error = %@",index,[error userInfo]);
+                DDLogError(@"Error removing report at index: %lu ; error = %@",(unsigned long)index,[error userInfo]);
             }
         }
         [_sortedNodes removeObjectAtIndex:index];
